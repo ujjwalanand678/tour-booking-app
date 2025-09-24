f0c3e78156387b73eb9618f927b50a142bb58b76
Ujjwal Anand <ujjwalanand678@gmail.com>
2025-09-24 16:57:06 +0530

    server created

---
+++ server/server.js
@@ -0,0 +1,23 @@
+import express from "express"
+import dotenv from "dotenv"
+import mongoose, { Mongoose } from "mongoose"
+
+const app = express()
+dotenv.config() 
+const port = process.env.PORT || 3000;
+mongoose.set("strictQuery", false)
+
+const connectDB = async() =>{
+    try {
+        mongoose.connect(process.env.MONGO_URI)
+        console.log(`Connected to MongoDB`)
+    } catch (error) {
+        console.log("Error")
+    }
+}
+
+connectDB().then(()=>{
+    app.listen(port, ()=>{
+    console.log(`server is running at ${port}`)
+})
+})

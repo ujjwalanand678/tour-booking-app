6d10c2811e25549d8956ddd4f7cb44ccb08ffe23
Ujjwal Anand <ujjwalanand678@gmail.com>
2025-09-26 00:50:59 +0530

    get all tour

--- server/controller/Tour.controller.js
+++ server/controller/Tour.controller.js
@@ -17,22 +17,43 @@ export const createTour = async (req, res, next) => {
   } = req.body;
 
   try {
-    const data = new Tour({ title,
-    city,
-    address,
-    distance,
-    price,
-    maxGroupSize,
-    description,
-    photo,
-    featured,
-    avgRating,
-    numReviews,})
+    const data = new Tour({
+      title,
+      city,
+      address,
+      distance,
+      price,
+      maxGroupSize,
+      description,
+      photo,
+      featured,
+      avgRating,
+      numReviews,
+    });
 
-    await data.save()
+    await data.save();
 
-    return res.status(200).json({success:true , message: "tour data added successfully"})
+    return res
+      .status(200)
+      .json({ success: true, message: "tour data added successfully" });
   } catch (error) {
-    return res.status(500).json({success:false, message:"internal server error, tour data was not added"})
+    return res
+      .status(500)
+      .json({
+        success: false,
+        message: "internal server error, tour data was not added",
+      });
   }
 };
+
+export const getAllTour = async (req, res, next) => {
+  try {
+    const allTour = await Tour.find();
+    return res
+      .status(200)
+      .json({ success: true, message: "all tours found", data: allTour });
+  } catch (error) {
+    return res.status(500).json({ success: false, message: "Server error..." });
+  }
+};
+
--- server/routes/Tour.routes.js
+++ server/routes/Tour.routes.js
@@ -1,10 +1,16 @@
 import express from "express"
-import { createTour } from "../controller/Tour.controller.js";
+import { createTour, deleteTourData, getAllTour } from "../controller/Tour.controller.js";
 
 
 const route = express.Router();
 
 //http://localhost:3000/worldtour/createtour
 route.post("/createtour" , createTour)
+
+//http://localhost:3000/worldtour/getalltour
+route.get("/getalltour", getAllTour)
+
+//http://localhost:3000/worldtour/deletetour/id
+route.delete("/deletetour/:id", deleteTourData)
 
 export default route

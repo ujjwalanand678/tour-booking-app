ef935b1eaebb1defe075a72f5617520383bfa91c
Ujjwal Anand <ujjwalanand678@gmail.com>
2025-09-26 14:35:23 +0530

    user login and jwt token done

--- server/controller/Auth.controller.js
+++ server/controller/Auth.controller.js
@@ -29,6 +29,12 @@ export const userRegister = async (req, res, next) => {
   }
 };
 
+const createJwtToken = (user)=>{
+return jwt.sign({id : user._id , role: user.role, name: user.name}, process.env.JWT_TOKEN_KEY,{expiresIn: "1d"}) 
+// it will create a token with the user id and role and name as payload and sign it with the secret key and set the expiry time to 1 day.
+// we can store any other information in the payload as well.
+}
+
 export const userLogin = async (req, res, next) => {
     //data aquired from the frontend/client/postman
  const {email,password}= req.body;
@@ -44,13 +50,14 @@ export const userLogin = async (req, res, next) => {
     if(!comparePassword){
         return res.status(400).json({ success: false, message: "Invalid password" });
     }
-
+const token = createJwtToken(user)
      // we dont want to send password and role in response. so we are using destructuring to exclude them. {...rest} will contain all the other fields except password and role.
      const { password: hashedPassword, role, ...rest}= user._doc; 
     return res
       .status(200)
       .json({ success: true, message: "User logged in successfully" , token , role, data: rest});
  } catch (error) {
-    
+    console.error("Login Error:", error);
+    return res.status(500).json({ success: false, message: "Server error" });
  }
 };

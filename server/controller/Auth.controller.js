3c20131f8858ebd2fd0017523eea71ab12da4ef5
Ujjwal Anand <ujjwalanand678@gmail.com>
2025-09-26 14:28:31 +0530

    user login almost done

--- server/controller/Auth.controller.js
+++ server/controller/Auth.controller.js
@@ -30,13 +30,26 @@ export const userRegister = async (req, res, next) => {
 };
 
 export const userLogin = async (req, res, next) => {
+    //data aquired from the frontend/client/postman
  const {email,password}= req.body;
  try {
+    //find the registered email
     const user = await User.findOne({email :email})
     if(!user){
         return res.status(400).json({success:false , message: "User is not registered"})
     }
-    
+
+    //compare password
+    const comparePassword = await bcrypt.compare(password, user.password) 
+    if(!comparePassword){
+        return res.status(400).json({ success: false, message: "Invalid password" });
+    }
+
+     // we dont want to send password and role in response. so we are using destructuring to exclude them. {...rest} will contain all the other fields except password and role.
+     const { password: hashedPassword, role, ...rest}= user._doc; 
+    return res
+      .status(200)
+      .json({ success: true, message: "User logged in successfully" , token , role, data: rest});
  } catch (error) {
     
  }

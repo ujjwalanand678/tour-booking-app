0a5a0b809943e8fd01395b82361892b9ed8c28c4
Ujjwal Anand <ujjwalanand678@gmail.com>
2025-09-28 20:12:29 +0530

    postman review error fixed

--- server/routes/Review.routes.js
+++ server/routes/Review.routes.js
@@ -11,15 +11,14 @@ import { authorize } from "../authentication/VerifyToken.js";
 const route = express.Router();
 
 // http://localhost:3000/worldtour/review/postreview
-route.post("postreview", authorize, createReview);
+route.post("/postreview", authorize, createReview);
 
 // http://localhost:3000/worldtour/review/getallreviews/12345
 route.get("/getallreviews/:tourId", getTourReviews);
 
 // http://localhost:3000/worldtour/review/deletereview/12345
 route.delete("/deletereview/:id", authorize, deleteReview);
-// If you want to restrict delete to admins only:
-// router.delete("/:id", authorize, authorizeRole("admin"), deleteReview);
+
 
 export default route;
 

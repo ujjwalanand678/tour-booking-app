dff6018fd0448efa6a3bd18d4199b5946a5b1808
Ujjwal Anand <ujjwalanand678@gmail.com>
2025-09-30 13:42:32 +0530

    coment

--- server/routes/Tour.routes.js
+++ server/routes/Tour.routes.js
@@ -9,6 +9,7 @@ const route = express.Router();
 //http://localhost:3000/worldtour/createtour
 route.post("/createtour" , createTour)
 
+//For pagination install mongoose-paginate-v2 package
 //http://localhost:3000/worldtour/tourpagination?page=1&limit=5
 route.get("/tourpagination",getTourPagination)
 

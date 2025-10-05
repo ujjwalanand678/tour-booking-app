ea339d8bd852d3d697115a49102d08ffcc8d3e54
Ujjwal Anand <ujjwalanand678@gmail.com>
2025-10-05 19:09:34 +0530

    stylings

--- client/src/Components/TourBookingForm.jsx
+++ client/src/Components/TourBookingForm.jsx
@@ -25,7 +25,7 @@ const TourBookingForm = () => {
   };
 
   return (
-    <div className="max-w-md mx-auto bg-white shadow-md rounded-lg p-6">
+    <div className="me-40 bg-white shadow-md rounded-lg p-6">
       <h2 className="text-lg font-semibold mb-4">Information</h2>
       <form onSubmit={handleSubmit} className="space-y-3">
         <input

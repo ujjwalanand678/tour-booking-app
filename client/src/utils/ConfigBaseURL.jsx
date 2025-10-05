33596c12ba707f7b19a31325c2d04e9c70c1d0e5
Ujjwal Anand <ujjwalanand678@gmail.com>
2025-10-05 18:43:31 +0530

    error handled

--- client/src/utils/ConfigBaseURL.jsx
+++ client/src/utils/ConfigBaseURL.jsx
@@ -1,3 +1,3 @@
-export const BASE_URL = "https://tour-booking-app-66f9.onrender.com"
+export const BASE_URL = "https://tour-booking-app-66f9.onrender.com/worldtour"
 
 export const token = localStorage.getItem("token");
